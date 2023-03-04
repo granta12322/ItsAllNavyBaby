11591133cc910242aafd75585346ece9fa6e7d50
Alex <alex.grant@bp.com>
2023-03-04 07:08:26 +0000

    Create sonar

---
+++ classes/Sonar/Sonar.ts
@@ -0,0 +1,9 @@
+class Sonar {
+    // Two modes, active and passive. 
+    // Active mode sends out a ping that gets reflected
+    //  This will require a depth modified sound profile coefficient
+    //  And alert everything to the fact that you're there
+    // Passive mode just listens for things
+    ping(): void {}
+    listen(): void {}
+}
